7b7dbad01ef5562c59176d92b69953c64c2d6b11
hjkim <guswns589898@gmail.com>
2022-08-19 14:39:47 +0900

    C++ 18258 : 큐 2

---
+++ OnlineJudge/18258.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main()
+{
+	cin.tie(0);
+	cin.sync_with_stdio(0);
+
+	int cnt, num;
+	string s;
+	queue<int> _queue;
+
+	cin >> cnt;
+
+	for (int i = 0; i < cnt; i++)
+	{
+		cin >> s;
+
+		if (s == "push")
+		{
+			cin >> num;
+			_queue.push(num);
+		}
+		else if (s == "pop")
+		{
+			if (!_queue.empty())
+			{
+				cout << _queue.front() << '\n';
+				_queue.pop();
+			}
+			else cout << -1 << '\n';
+		}
+		else if (s == "size")  cout << _queue.size() << '\n';
+		else if (s == "empty") cout << _queue.empty() << '\n';
+		else if (s == "front")
+		{
+			if (!_queue.empty()) cout << _queue.front() << '\n';
+			else cout << -1 << '\n';
+		}
+		else 
+		{
+			if (!_queue.empty()) cout << _queue.back() << '\n';
+			else cout << -1 << '\n';
+		}
+	}
+
+}
