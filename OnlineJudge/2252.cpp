2fb88b8eb99f494a226be16bf6f1b2c6a7e3200e
hjkim <guswns589898@gmail.com>
2022-10-06 16:51:18 +0900

    C++ 2252 : 줄세우기

--- OnlineJudge/2252.cpp
+++ OnlineJudge/2252.cpp
@@ -1,13 +1,48 @@
 #include <iostream>
-#include <queue>
-#include <vector>
+#include<queue>
+#include<vector>
 
 using namespace std;
 
-int main()
-{
-	int N, M;
+int main() {
 
-	int degree[32000] = { 0, };
-	vector<int> vec;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int degree[32000];
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> vec(32000);
+    queue<int> q;
+
+    for (int i = 1; i <= m; i++) {
+        int a, b;
+        cin >> a >> b;
+        degree[b]++;
+        vec[a].push_back(b);
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (degree[i] == 0) {
+            q.push(i);
+        }
+    }
+
+    while (!q.empty()) {
+
+        int qNum = q.front();
+        q.pop();
+        cout << qNum << ' ';
+
+        for (int i = 0; i < vec[qNum].size(); i++) {
+
+            int next = vec[qNum][i];
+            if (--degree[next] == 0) {
+                q.push(next);
+            }
+
+        }
+
+    }
 }
