8754c6b6fb4cfedb2b8a3b908235dac5a056f9f1
hjkim <guswns589898@gmail.com>
2022-10-19 13:38:23 +0900

    C++ 17610 : 양팔 저울

---
+++ OnlineJudge/17610.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+bool _isExist[10000000];
+int _gi[14];
+int k;
+int sum;
+int result;
+
+void recursive(int idx, int input)
+{
+	if (input >= 1)
+	{
+		if (!_isExist[input])
+			result++;
+		_isExist[input] = true;
+	}
+
+	if (idx == k)
+		return;
+
+	recursive(idx + 1, input + _gi[idx]);
+	recursive(idx + 1, input - _gi[idx]);
+	recursive(idx + 1, input);
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> k;
+
+	for (int i = 0; i < k; i++)
+	{
+		cin >> _gi[i];
+
+		sum += _gi[i];
+	}
+
+	recursive(0, 0);
+
+	cout << sum - result << "\n";
+}
