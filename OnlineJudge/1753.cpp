1f2d63044041d17e01562357b82eb928162cd84c
hjkim <guswns589898@gmail.com>
2022-11-16 17:27:30 +0900

    C++ 1753 : 최단경로 (우선순위큐)

--- OnlineJudge/1753.cpp
+++ OnlineJudge/1753.cpp
@@ -1,45 +1,81 @@
-// 최단경로
-// 그래프 이론, 다익스트라
-#include<iostream>
-#include<vector>
-#include<queue>
-#define INF 2147000000
-#define MAX 20001
+#include <iostream>
+#include <vector>
+#include <queue>
+#define INF 1000000
+#define NODE_MAX 20001
+#define VERTEX_MAX 300001
+
 using namespace std;
-struct Edge {
-    int ver;
-    int dist;
-    Edge(int a, int b) :ver(a), dist(b) {};
-    bool operator<(const Edge& e)const { return dist > e.dist; }
-};
-priority_queue<Edge>q;
-vector<pair<int, int>>g[MAX];
-vector<int>dist(MAX, INF);
-void dijkstra(int x) {
-    dist[x] = 0;
-    q.push(Edge(x, 0));
-    while (!q.empty()) {
-        int v = q.top().ver;
-        int d = q.top().dist;
-        q.pop();
-        for (int i = 0; i < g[v].size(); i++) {
-            int nextV = g[v][i].first;
-            int nextD = d + g[v][i].second;
-            if (dist[nextV] > nextD) {
-                dist[nextV] = nextD;
-                q.push(Edge(nextV, nextD));
-            }
-        }
-    }
+
+int V, E, K;
+int minVal[NODE_MAX];
+
+// pair <가중치, 도착노드>
+vector<pair<int, int>> vec[VERTEX_MAX];
+priority_queue<pair<int, int>> pq;
+
+void Dijkstra(int idx)
+{
+	int NowIdx = 0;
+	int weight = 0;
+	// 자기 자신으로 가는 가중치는 없음.
+	minVal[idx] = 0;
+
+	pq.push(make_pair(0, idx));
+
+	while (!pq.empty())
+	{
+		NowIdx = pq.top().second;
+		weight = -pq.top().first;
+
+		pq.pop();
+
+		if (minVal[NowIdx] < weight)
+			continue;
+
+		for (int i = 0; i < vec[NowIdx].size(); i++)
+		{
+			int next_node = vec[NowIdx][i].second;
+			int total_weight = weight + vec[NowIdx][i].first;
+
+			if (minVal[next_node] > total_weight)
+			{
+				minVal[next_node] = total_weight;
+
+				pq.push(make_pair(-total_weight, next_node));
+			}
+		}
+	}
 }
-int main() {
-    int V, E, K, a, b, c;
-    cin >> V >> E >> K;
-    for (int i = 0; i < E; i++) {
-        cin >> a >> b >> c;
-        g[a].push_back({ b,c });
-    }
-    dijkstra(K);
-    for (int i = 1; i <= V; i++)
-        cout << (dist[i] == INF ? "INF" : to_string(dist[i])) << '\n';
+
+int main()
+{
+	// 시작노드, 도착노드, 가중치
+	int u, v, w;
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> V >> E;
+	cin >> K;
+
+	fill(minVal, minVal + V + 1, INF);
+
+	for (int i = 0; i < E; i++)
+	{
+		cin >> u >> v >> w;
+
+		//시작노트 vec에 도착노드로 가는 가중치를 같이 저장
+		vec[u].push_back(make_pair(w, v));
+	}
+
+	Dijkstra(K);
+
+	for (int i = 1; i < V + 1; i++)
+	{
+		if (minVal[i] == INF)
+			cout << "INF" << "\n";
+		else
+			cout << minVal[i] << "\n";
+	}
 }
