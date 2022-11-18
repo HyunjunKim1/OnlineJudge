b717e507dce66257cef50a0c66a2aaf3e340eb3a
hjkim <guswns589898@gmail.com>
2022-11-18 16:17:36 +0900

    1916 중간푸시

--- OnlineJudge/1916.cpp
+++ OnlineJudge/1916.cpp
@@ -1,55 +1,50 @@
 #include <iostream>
-#include <queue>
 #include <vector>
-#include <algorithm>
+#include <queue>
+#define MAX 1001
+#define INF 1000000
 
 using namespace std;
-int n, m;
-int dp[1001];
-vector<pair <int, int>> road[1001];
 
-void bfs(int start) {
+int N, M;
+int minCost[MAX];
+vector<pair<int, int>> vec[MAX];
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    pq.push({ 0, start });
+void Dijkstra(int idx)
+{
+	minCost[idx] = 0;
+	pq.push({ 0,idx });
 
-    while (!pq.empty()) {
-        int current = pq.top().second;
-        int cost = pq.top().first;
-        pq.pop();
-        if (cost > dp[current]) {
-            continue;
-        }
-
-        for (int i = 0; i < road[current].size(); i++) {
-            int next = road[current][i].first;
-            int next_cost = dp[current] + road[current][i].second;
-
-            if (next_cost < dp[next]) {
-                dp[next] = next_cost;
-                pq.push({ next_cost,next });
-            }
-        }
-    }
+	while (!pq.empty()) 
+	{
+		int NowIdx = pq.top().second;
+		int cost = pq.top().first;
+	}
 }
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    cin >> n >> m;
+	cin >> N;
+	cin >> M;
 
-    for (int i = 0; i < m; i++) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        road[a].push_back({ b,c });
-        //a 시작 b도착 c금액
-    }
-    int start, end;
-    cin >> start >> end;
-    fill_n(dp, 1001, 987654321);
-    dp[start] = 0;
-    bfs(start);
+	int start, end, cost;
 
-    cout << dp[end];
+	for (int i = 0; i < M; i++)
+	{
+		cin >> start >> end >> cost;
+		vec[start].push_back({ end,cost });
+	}
+
+	int startNode, endNode;
+
+	cin >> startNode >> endNode;
+
+	fill(minCost, minCost + MAX, INF);
+
+	Dijkstra(startNode);
+
+	cout << minCost[endNode];
 }
