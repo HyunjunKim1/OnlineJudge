bc27f0f9bcf53734ba585fee773a0cbc8dfc139d
hjkim <guswns589898@gmail.com>
2023-01-02 17:25:06 +0900

    C++ 1463 : 1로 만들기

---
+++ OnlineJudge/1463.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#define MAX 1000001
+using namespace std;
+
+int N;
+int dp[MAX];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N;
+
+	int i = 1;
+	while (i++ < MAX)
+	{
+		dp[i] = dp[i - 1] + 1;
+		
+		if (i % 2 == 0)
+		{
+			if (dp[i] >= dp[i / 2] + 1)
+				dp[i] = dp[i / 2] + 1;
+		}
+		if (i % 3 == 0)
+		{
+			if (dp[i] >= dp[i / 3] + 1)
+				dp[i] = dp[i / 3] + 1;
+		}
+	}
+
+	cout << dp[N];
+}
