b91170ba47e6ee1f343743f3b231e19d83f8186f
HyunjunKim1 <guswns5898@naver.com>
2024-02-02 11:24:03 +0900

    Python 2869 달팽이

---
+++ OnlineJudge/2869.py
@@ -0,0 +1,18 @@
+#A, B, V = map(int, input().split())
+#result = 0
+#cnt = 0
+#while(True):
+#    cnt += 1
+#    result += A
+#    if result >= V:
+#        break;
+#    result -= B
+#    
+#print(cnt#########)
+
+A, B, V = map(int, input().split())
+
+if (V-B) % (A-B) == 0:
+    print((V-B)//(A-B))
+else:
+    print((V-B)//(A-B)+1)
