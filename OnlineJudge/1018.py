5dd56623a890ed9d331b98a340605e89b0e42ec1
HyunjunKim1 <guswns5898@naver.com>
2024-02-07 09:36:51 +0900

    Python 1018, 19532 브루트포스알고리즘

---
+++ OnlineJudge/1018.py
@@ -0,0 +1,28 @@
+N, M = map(int, input().split())
+board = []
+result = []
+
+for i in range(N):
+    board.append(input())
+    
+for i in range(N-7):
+    for j in range(M-7):
+        draw1 = 0
+        draw2 = 0
+        for a in range(i, i+8):
+            for b in range(j, j+8):
+                if (a+b) % 2 == 0:
+                    if board[a][b] != 'B':
+                        draw1 += 1
+                    if board[a][b] != 'W':
+                        draw2 += 1
+                else:
+                    if board[a][b] != 'W':
+                        draw1 += 1
+                    if board[a][b] != 'B':
+                        draw2 += 1
+                        
+        result.append(draw1)
+        result.append(draw2)
+
+print(min(result))
