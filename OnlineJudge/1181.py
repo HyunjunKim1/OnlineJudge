169cc25fab82a509e733dd3b90598cd50acd987a
HyunjunKim1 <guswns5898@naver.com>
2024-02-14 11:35:42 +0900

    Python 1181 단어정렬 풀이 2개

--- OnlineJudge/1181.py
+++ OnlineJudge/1181.py
@@ -1,4 +1,26 @@
-N = int(input())
+#N = int(input())
 
-for _ in range(N):
-    
+#str_list = []
+#for _ in range(N):
+#    str_list.append(str(input()))
+
+#str_list = list(set(str_list))
+#str_list.sort()
+
+#for i in range(len(str_list)):
+#    for j in range(i,len(str_list)):
+#        if len(str_list[i]) > len(str_list[j]): 
+#            str_list[i], str_list[j] = str_list[j],str_list[i]
+
+#for i in range(len(str_list)):
+#    print(str_list[i])
+n = int(input())
+
+words = [str(input()) for i in range(n)]
+
+words = list(set(words))
+words.sort()
+words.sort(key=len)
+
+for i in words:
+    print(i)
