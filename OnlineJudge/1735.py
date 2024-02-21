844bb0c4c7c2ee25475c7bd22154ce5efb274eb1
HyunjunKim1 <guswns5898@naver.com>
2024-02-21 16:26:24 +0900

    Python 28278

--- OnlineJudge/1735.py
+++ OnlineJudge/1735.py
@@ -1,18 +1,12 @@
 def gcd(x,y):
-    mod = x % y 
-    
-    while mod > 0:
+    mod = x % y
+    while mod >0:
         x = y
         y = mod
         mod = x % y
-    return y
+    return y    
 
 A, B = map(int, input().split())
 C, D = map(int, input().split())
-
-N = gcd(A*D + C*B, B*D)
-
-print((A*D + C*B)/N,B*D//N)
-    
-    
-    
+N = gcd(A*D + C*B, B*D) 
+print((A*D + C*B)//N, B*D//N)
