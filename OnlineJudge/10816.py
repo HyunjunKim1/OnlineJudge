83f62ae5bc1ac60252bbe0e977a425ac444f8637
HyunjunKim1 <guswns5898@naver.com>
2024-04-12 15:40:06 +0900

    Python 10816 숫자카드 2

--- OnlineJudge/10816.py
+++ OnlineJudge/10816.py
@@ -1,10 +1,21 @@
-M = int(input())
-have_count = []
-
-for _ in range(M):
-    have_count.append(int(input().split()))
+import sys
+input = sys.stdin.readline
 
 N = int(input())
-check_count = []
+cards = [*map(int, input().split())]
+M = int(input())
+candidate = [*map(int, input().split())]
 
-for 
+count = {}
+for card in cards:
+    if card in count:
+        count[card] += 1
+    else:
+        count[card] = 1
+
+for target in candidate:
+    result = count.get(target)
+    if result == None:
+        print(0, end=" ")
+    else:
+        print(result, end=" ")
