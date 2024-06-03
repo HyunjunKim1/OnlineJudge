aabf5ee42583413cf4301dd7269f571d595e5ab2
HyunjunKim1 <guswns5898@naver.com>
2024-06-04 08:39:32 +0900

    14502 파이썬 벽뿌사뿌기

---
+++ OnlineJudge/14502.py
@@ -0,0 +1,52 @@
+from collections import deque
+import copy
+n, m = map(int, input().split())
+graph = []
+
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+
+def bfs():
+    q = deque
+    tmp_graph = copy.deepcopy(graph)
+    
+    for i in range(n):
+        for j in range(m):
+            if tmp_graph[i][j] == 2:
+                q.append((i,j))
+                
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 0 <= nx < n and 0 <= ny < m:
+                if tmp_graph[nx][ny] == 0:
+                    tmp_graph[nx][ny] = 2
+                    q.append((nx, ny))
+    
+    global answer
+    cnt = 0
+    for i in range(n):
+        cnt += tmp_graph[i].count(0)
+        
+    answer = max(answer, cnt)
+    
+def makeWall(cnt):             
+    if cnt == 3:
+        bfs()
+        return
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 0:
+                graph[i][j] = 1
+                makeWall(cnt+1)
+                graph[i][j] = 0
+                
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+    
+answer = 0
+makeWall(0)
+print(answer)
