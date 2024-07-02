9ff8bafc0125aa0cbce145a8fd7373b1782b166c
HyunjunKim1 <guswns5898@naver.com>
2024-07-02 14:39:32 +0900

    1951 활자

---
+++ OnlineJudge/1951.py
@@ -0,0 +1,34 @@
+#import sys
+#
+#input = sys.stdin.readline
+#
+#N = int(input())
+#num = ""
+#
+#for i in range(1, N + 1):
+#    num += f"{i}"
+#
+#num_list = list(num)
+#
+#print(len(num_list))
+n = int(input())
+s = [0 for i in range(10)]
+point = 1
+while n != 0:
+    while n % 10 != 9:
+        for i in str(n):
+            s[int(i)] += point
+        n -= 1
+    if n < 10:
+        for i in range(n + 1):
+            s[i] += point
+        s[0] -= point
+        break
+    else:
+        for i in range(10):
+            s[i] += (n // 10 + 1) * point
+    s[0] -= point
+    point *= 10
+    n //= 10
+
+print(sum(s) % 1234567)
